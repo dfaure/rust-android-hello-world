5e7fc2a0383208ab53506bbeb5a50f95a60301cc
David Faure <faure@kde.org>
2025-07-25 00:31:16 +0200

    speed up android build

--- build.rs
+++ build.rs
@@ -1,10 +1,13 @@
 fn main() {
     let config = slint_build::CompilerConfiguration::new()
-    //.with_style("fluent-light".into());
-    .with_style("material-light".into());
+    .with_style("fluent-light".into());
+    //.with_style("material-light".into());
+    //.with_style("material-dark".into());
+    //.with_style("cupertino-dark".into());
+    //.with_style("cosmic-light".into());
 
     // material-light: too big
-    // fluent-light: clean, very sqaure, blue highlight below the lineedit, blue selection
+    // fluent-light: clean, very square, blue highlight below the lineedit, blue selection
     // cupertino-light: even smaller, clean too, blue highlight around the lineedit
     // cosmic-light: too gray
 
