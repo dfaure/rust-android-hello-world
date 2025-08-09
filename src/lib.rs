2259b70d705cc6d8762ab623ea4209e471ae8d19
David Faure <faure@kde.org>
2025-08-09 23:46:44 +0200

    Fix second run on Android

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,10 @@ fn android_main(app: slint::android::AndroidApp) -> Result<(), Box<dyn Error>> {
 
     slint::android::init(app).unwrap();
     log::info!("slint::android initialized");
-    real_main()
+    real_main();
+
+    // When we get here, exit process so Android restarts fresh next time
+    std::process::exit(0);
 }
 
 pub fn real_main() -> Result<(), Box<dyn Error>> {
