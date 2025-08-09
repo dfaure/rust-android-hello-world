e432d6f514a415d57e1ef13aa0e76d64b0de146c
David Faure <faure@kde.org>
2025-08-09 23:46:44 +0200

    Test reqwest

--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,34 @@ fn android_main(app: slint::android::AndroidApp) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn real_main() -> Result<(), Box<dyn Error>> {
-    log::info!("Rust Android Hello World");
-    let ui = AppWindow::new()?;
-    ui.run()?;
+    log::info!("real_main calling test_reqwest");
+    //let ui = AppWindow::new()?;
+    //ui.run()?;
+    if let Err(e) = test_reqwest() {
+        log::warn!("Error: {:?}", e);
+    }
+    Ok(())
+}
+
+pub fn test_reqwest() -> Result<(), Box<dyn Error>> {
+    log::info!("test_main: starting");
+    let slint_future = async move {
+        log::info!("test_main (future): running");
+        match reqwest::get("http://httpbin.org/range/26").await {
+            Ok(response) => {
+                log::info!("download worked; {}", response.text().await.unwrap());
+            },
+            Err(e) => {
+                log::error!("download error: {:?}", e);
+            }
+        }
+        log::info!("test_main (future): calling quit_event_loop");
+        slint::quit_event_loop().unwrap();
+    };
+    log::info!("test_main: future created");
+    // Spawn the future on Slint's event loop
+    slint::spawn_local(async_compat::Compat::new(slint_future))?;
+    log::info!("test_main: after spawn_local");
+    slint::run_event_loop_until_quit()?;
     Ok(())
 }
